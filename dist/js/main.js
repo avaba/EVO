0d70010ff1279268023c85be32a49d21404602ae
avaba <42552832+avaba@users.noreply.github.com>
2020-10-01 14:14:53 +0300

    header

--- dist/js/main.js
+++ dist/js/main.js
@@ -166,15 +166,6 @@
   $('.box__link').on('click', function (e) {
     $(this).closest('.box').toggleClass('open');
   });
-  $(document).mouseup(function (e) {
-    // событие клика по веб-документу
-    var div = $('.box__hide'); // тут указываем класс элемента
-
-    if (!div.is(e.target)) {
-      // и не по его дочерним элементам
-      div.closest('.box').removeClass('open'); // скрываем его
-    }
-  });
 });
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
 
